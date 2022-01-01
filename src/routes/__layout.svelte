e770f18a100c1917583ae6a5feedc31504632a34
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-01-01 20:05:36 +0100

    responsive design

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -11,8 +11,6 @@
     $: hamburger = "hidden";
     $: blur = "blur-none";
 
-    $: console.log(blur);
-
     const toggleHamburger = () => {
         if (hamburger === "hidden") {
             hamburger = "block";
@@ -33,14 +31,14 @@
         <h1 class="text-3xl font-semibold text-bee my-auto">Rando Norris</h1>
         <div class="links text-marine ml-auto hidden lg:flex">
             {#each nav as item}
-                <a class:active={$page.path === item.path} class="rounded-full text-white border-2 border-bee p-1.5 ml-2 focus:bg-bee focus:text-marine focus:outline-none" href={item.path}>{item.title}</a>    
+                <a class:active={$page.path === item.path} class="rounded-full text-white border-2 border-bee p-1.5 ml-2 focus:bg-bee focus:text-marine focus:outline-none" href={item.path} on:click={toggleHamburger}>{item.title}</a>    
             {/each}
         </div>
         <div class="hamburger ml-auto lg:hidden">
             <img class="cursor-pointer mx-auto" src="/menu.svg" alt="menu" width="50px" style="color: white" on:click={toggleHamburger}>
             <div class="absolute blur-none z-10 bg-marine-bright top-24 right-3 p-5 rounded-2xl {hamburger}">
                 {#each nav as item}
-                    <div class="my-5"><a class:activeMobile={$page.path === item.path} class="text-white px-4 focus:outline-none focus:underline" href={item.path}>{item.title}</a></div>
+                    <div class="my-5"><a class:activeMobile={$page.path === item.path} class="text-white px-4 focus:outline-none focus:underline" href={item.path} on:click={toggleHamburger}>{item.title}</a></div>
                 {/each}
             </div>
         </div>

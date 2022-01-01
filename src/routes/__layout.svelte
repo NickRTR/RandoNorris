1d5dd71eca13aec5e9f96a403c137c5028021575
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-01-01 18:03:03 +0100

    Hamburger on mobile

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,15 +7,33 @@
         {title: "Decision", path: "/randomdecision"},
         {title: "Letter", path: "/randomletter"},
     ];
+
+    $: hamburger = "hidden";
+
+    const toggleHamburger = () => {
+        if (hamburger === "hidden") {
+            hamburger = "block";
+        } else {
+            hamburger = "hidden";
+        }
+    }
 </script>
 
 <body class="bg-marine text-center min-h-screen text-xl">
-    <header class="bg-marine-bright flex sm:px-10 md:px-40 lg:px-60 py-4 mb-6">
-        <h1 class="text-2xl sm:text-4xl mb-1 font-semibold text-bee">Rando Norris</h1>
-        <div class="links text-marine ml-auto my-auto">
+    <header class="bg-marine-bright flex px-2.5 sm:px-20 md:px-40 lg:px-60 py-4 mb-6">
+        <h1 class="text-3xl font-semibold text-bee my-auto">Rando Norris</h1>
+        <div class="links text-marine ml-auto hidden lg:flex">
             {#each nav as item}
-                <a class:active={$page.path === item.path} class="rounded-full text-white border-2 border-bee p-1.5 ml-2" href={item.path}>{item.title}</a>    
+                <a class:active={$page.path === item.path} class="rounded-full text-white border-2 border-bee p-1.5 ml-2 focus:bg-bee focus:text-marine focus:outline-none" href={item.path}>{item.title}</a>    
             {/each}
+        </div>
+        <div class="hamburger ml-auto lg:hidden">
+            <img class="cursor-pointer mx-auto" src="/menu.svg" alt="menu" width="40px" style="color: white" on:click={toggleHamburger}>
+            <div class="relative rounded-2xl mx-auto {hamburger}">
+                {#each nav as item}
+                    <div class="my-5"><a class:activeMobile={$page.path === item.path} class="text-white px-4 focus:outline-none focus:underline" href={item.path}>{item.title}</a></div>
+                {/each}
+            </div>
         </div>
     </header>
 
@@ -34,5 +52,10 @@
     .active {
         background-color: #F6C90E;
         color: #303841;
+    }
+
+    .activeMobile {
+        color: #F6C90E;
+        text-decoration-line: underline;
     }
 </style>

b67d5242b3456e7359a4999e94f6003c63a37bac
Nick <95316598+NickRTR@users.noreply.github.com>
2023-07-30 00:29:43 +0200

    Wipe project and init new SvelteKit project

--- src/lib/Button.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-    export let action;
-    export let text;
-</script>
-
-<button class="text-marine px-3 py-1.5 mb-4 font-bold mt-5 bg-bee rounded-full" type="submit" on:click|preventDefault={action}>{text}</button>
